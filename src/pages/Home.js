8c254db4a34f60f4521fd1cbbb14679093c10e36
saikat samanta <saikat.samanta@yapsody.com>
2024-01-12 11:29:54 +0530

    to-do added list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,26 @@
 import Task from "../Components/Task";
 import {useState} from 'react';
 export default function Home() {
-  const tasklist = [
+  const [tasklist,setTasklist] = useState([
     "Work on bug #21",
     "Api call",
     "Check with FE",
     "Machine learning algo",
-  ];
+  ]);
+  const [inp,setInp] = useState("")
   const [count, setCount] = useState(0);
-  const incfun=()=>{setCount(count+1)}
+ 
   return (
     <center>
       <h1>React To-do 🌟</h1>
-      <input type="text" className="inp" />
-      <button id="add">Add</button>
+      <input type="text" className="inp" onChange={(e)=>{setInp(e.target.value)}} />
+      <button id="add" onClick={()=>setTasklist([...tasklist, inp])}>Add</button>
       {tasklist.map((v, i) => (
         <Task key={i} data={v} />
       ))}
       <h1> {count} </h1>
-      <button onClick={incfun}>Inc</button>
+      <button onClick={()=>{setCount(count+1)}}>Inc</button>
+      <button onClick={()=>{setCount(count-1)}}>Dec</button>
     </center>
   );
 }
